f80d2416bfd103fa0e7584a71db75dc98f906ad4
AndrewKamenev <ancam@yandex.ua>
2016-03-22 21:17:50 +0200

    Version2.0

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
 	$(document).ready(function(){
+		function validate(){
+			if($('input').val().match(/[0-9,\,]/)){
+					var arrayToSort = $('input').val().split(',').map(function(i){
+						return parseInt(i, 10);
+					});
+				return array = arrayToSort;
+				}
+				else {
+					alert('Enter only digits and comas');
+				}
+		}
 		function sort(array){
-			for(var j = 0; j < array.length+1; j++){
-				for (var i = 0; i < array.length+1; i++) {
+			for(var j = 0; j < array.length-1; j++){
+				for (var i = 0; i < array.length-1; i++){
 					if(array[i] > array[i + 1]){
 						var temp = array[i]; 
 						array[i] = array[i + 1];
@@ -24,19 +35,16 @@
 		};
 		function refresh(){
 			$('#forArray').html('');
-			var arrayToSort = prompt('Enter digits to sort. Separate them by coma').split(',').map(function(i){
-				return parseInt(i, 10);
-			});	
-			drawArray(arrayToSort);
-			console.log(arrayToSort);
-			return arrayToSort;
+			$('input').val('');	
 		};
 	
 		$('#sort').on('click', function(ev){
+			validate();
+			drawArray(array);
 			sort(array);
 		});
 		$('#refresh').on('click', function(ev){
 			return array = refresh();	
 		});
-	var array = refresh();
+	// var array = refresh();
 	});
